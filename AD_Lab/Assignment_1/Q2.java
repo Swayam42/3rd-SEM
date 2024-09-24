10ea744f3af4da745c5ed454693273904e1c6439
Swayam <89599729+Swayam42@users.noreply.github.com>
2024-09-24 22:33:14 +0530

    Add files via upload

---
+++ AD_Lab/Assignment_1/Q2.java
@@ -0,0 +1,19 @@
+/*find max and min elements in an array */
+
+public class Q2 {
+    public static void main(String[] args) {
+        int a[]={23,53,12,32};
+        int max=a[0];
+        int min=a[0];
+        for(int i=1;i<a.length;i++){
+            if(max<a[i]){
+                max=a[i];
+            }
+            if(a[i]<min){
+                min=a[i];
+            }
+        }
+        System.out.println("Max num: "+max);
+        System.out.println("Min num: "+min);
+    }
+}
